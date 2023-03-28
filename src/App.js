00faf1b41b27ef2e3f87ad4efbe5863bae457f8c
rajbalaraj <rajbalasr15@gmail.com>
2023-03-28 11:37:35 +0600

    increase-decarese

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter></Counter>
+
     </div>
   );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  const handleIncrease = () => setCount(count + 1)
+  const handleDecrease = () => setCount(count - 1)
+
+  return (
+    <div>
+      <h1>Counter:{count}</h1>
+      <button onClick={handleIncrease
+      }>Increase:</button>
+      <button onClick={handleDecrease}>Decrease</button>
+    </div>
+  )
 }
 
 export default App;
